f0f7bcfdada664930c101799968a4e78328d5e8b
radjikinseptiawan <radjikin21@gmail.com>
2024-11-15 16:40:54 +0700

    Membuat server

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import daStyle from 'eslint-config-dicodingacademy';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [daStyle,
+  {
+    files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' }
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+];
